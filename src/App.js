5ba428a0390eb8e633fb056c9cfa778c71d1e3d5
hemantrk2122 <hemantrk2122@gmail.com>
2023-07-08 15:58:45 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [code, setCode] = useState('');
+  const [output, setOutput] = useState('');
+
+  const handleSubmit = async () => {
+    const payload = {
+      language: 'cpp',
+      code
+    };
+
+    try {
+      const { data } = await axios.post('http://localhost:4000/run', payload);
+      // console.log(data);
+      setOutput(data.output);
+    } catch (error) {
+      setOutput(error.response.data.error.stderr)
+      // console.log(error.response.data.error.stderr);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Online Code Compiler</h1>
+      {/* <select className="select-box">
+        <option value='cpp'>C++</option>
+        <option value='c'>C</option>
+        <option value='py'>Python</option>
+        <option value='java'>Java</option>
+      </select>
+      <br /> */}
+      <textarea rows='20' cols='75' className="textarea" value={code} onChange={(e) => {
+        setCode(e.target.value);
+      }}></textarea>
+      <br />
+      <button className="submit-button" onClick={handleSubmit}>
+        Submit
+      </button>
+      {output &&
+        <div className="outputbox">
+          <p>{output}</p>
+        </div>
+      }
     </div>
   );
 }
